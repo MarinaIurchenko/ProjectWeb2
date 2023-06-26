455b21de8dc8df018560f93ccd0ed22041ae88c2
Marina Iurchenko <lamanch1203@gmail.com>
2023-06-26 11:15:45 +0300

    test modified

--- src/test/java/ru/netology/web/DeliveryCardTest.java
+++ src/test/java/ru/netology/web/DeliveryCardTest.java
@@ -1,18 +1,12 @@
 package ru.netology.web;
 
-
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
-
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
@@ -20,30 +14,29 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryCardTest {
 
-    @BeforeEach
-    void setUp() {
-        Configuration.headless = true;
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        open("http://localhost:9999/");
 
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     @Test
     void cardDeliveryArrangement() {
+        open("http://localhost:9999/");
 
         $("[data-test-id=city] input").setValue("Самара");
-        LocalDate date = LocalDate.of(2023, 10, 25);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("25.10.2023");
-        String formattedDate = date.format(formatter);
+        String planningDate = generateDate(5);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("01.07.2023");
         $("[data-test-id=name] input").setValue("Кузнецов Анатолий");
         $("[data-test-id=phone] input").setValue("+77468536452");
         $(".checkbox__box").click();
         $("button").click();
         $(byText("Забронировать")).click();
         $(withText(" Успешно!")).shouldBe(Condition.hidden, Duration.ofSeconds(5));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
 
     }
